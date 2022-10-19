d94c88cfb9b1dbb83f054e6aa03a6a2e0480c79c
MiersX <michael.muirhead123@gmail.com>
2022-10-19 06:55:42 +0000

    Create queries for 6 and 7. Validate employees, stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Michael", last_name: "Muirhead", hourly_rate: 60)
+@store1.employees.create(first_name: "Jeff", last_name: "Bondai", hourly_rate: 50)
+@store1.employees.create(first_name: "Michelle", last_name: "Elliot", hourly_rate: 85)
+@store1.employees.create(first_name: "Alix", last_name: "Faraize", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Carla", last_name: "Davis", hourly_rate: 45)
+@store2.employees.create(first_name: "Naomi", last_name: "Bridges", hourly_rate: 55)
+@store2.employees.create(first_name: "Carlos", last_name: "Hopper", hourly_rate: 65)
+@store2.employees.create(first_name: "John", last_name: "Tsu", hourly_rate: 125)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+user_answer = $stdin.gets.chomp
+@new_store = Store.create(name: "#{user_answer}")
+if @new_store.errors.any?
+  @new_store.errors.each do |error|
+    puts error.attribute, error.message
+  end
+end
